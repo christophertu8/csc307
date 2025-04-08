7fd55215ca8641b1af3b690ed8975efc224f36a8
christophertu8 <ctu07@calpoly.edu>
2025-04-07 17:56:05 -0700

    commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,19 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
+import React, { useState } from "react";
+
+function Form() {
+  const [person, setPerson] = useState({
+    name: "",
+    job: ""
+  });
+}
+export default Form;
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -36,8 +28,7 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
+      <Form />
     </div>
   );
 }
-
-export default MyApp;
