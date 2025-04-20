46064758af1a5461966afd7fad8b9518433ee59d
christophertu8 <ctu07@calpoly.edu>
2025-04-19 20:42:55 -0700

    Final commit: linked frontend and backend with GET, POST, DELETE

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,7 +1,7 @@
 // src/MyApp.jsx
 import Table from "./Table";
 import Form from "./Form";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default MyApp;
 
@@ -9,15 +9,69 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
+    const userToDelete = characters[index];
+  
+    fetch(`http://localhost:8000/users/${userToDelete.id}`, {
+      method: "DELETE"
+    })
+      .then((res) => {
+        if (res.status === 204) {
+          // Update frontend only if backend deletion succeeded
+          const updated = characters.filter((_, i) => i !== index);
+          setCharacters(updated);
+        } else {
+          throw new Error("Delete failed");
+        }
+      })
+      .catch((error) => {
+        console.log("DELETE failed:", error);
+      });
   }
+  
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json(); // get the created user w/ ID
+        } else {
+          throw new Error("Failed to add user");
+        }
+      })
+      .then((newUser) => {
+        setCharacters([...characters, newUser]);
+      })
+      .catch((error) => {
+        console.log("POST failed:", error);
+      });
   }
+  
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+  
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }  
 
   return (
     <div className="container">
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -3,6 +3,7 @@ function TableHeader() {
     return (
       <thead>
         <tr>
+          <th>ID</th>
           <th>Name</th>
           <th>Job</th>
           <th>Delete</th>
@@ -15,6 +16,7 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
+          <td>{row.id}</td>
           <td>{row.name}</td>
           <td>{row.job}</td>
           <td>
